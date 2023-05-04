a77b4e584dd835e39ffc7483a556ce336fb2fa9c
Moa Junior <moacirfonsecajr@gmail.com>
2023-05-04 16:38:32 -0300

    Project begins

---
+++ GetItDone/GetItDone/GetItDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetItDoneApp.swift
+//  GetItDone
+//
+//  Created by Moa Camilo on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GetItDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
